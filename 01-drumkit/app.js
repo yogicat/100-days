a488cb6516e4665b122f7f338cc195817d4b750c
yogicat <dahe.oh@gmail.com>
2018-02-14 15:32:49 +0900

    add new project

--- 01-drumkit/app.js
+++ 01-drumkit/app.js
@@ -1,24 +1,24 @@
+var init = (function(){
+  function playSound(e) {
+    var audio = document.querySelector(`audio[data-key="${e.which}"]`);
+    var key = document.querySelector(`.key[data-key="${e.which}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('isPlaying');
+    // document.querySelector('h1').innerHTML = e.key;
+    document.querySelector('header').classList.add('fade');
+  }
 
-function playSound(e) {
-  var audio = document.querySelector(`audio[data-key="${e.which}"]`);
-  var key = document.querySelector(`.key[data-key="${e.which}"]`);
-  if (!audio) return;
-  audio.currentTime = 0;
-  audio.play();
-  key.classList.add('isPlaying');
-  document.querySelector('h1').classList.add('fade');
-}
+  function removeClass(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('isPlaying')
+  }
 
-function removeClass(e) {
-  if (e.propertyName !== 'transform') return;
-  this.classList.remove('isPlaying')
-}
+  var keys = document.querySelectorAll('.key');
+  keys.forEach(function (key) {
+    key.addEventListener('transitionend', removeClass)
+  })
 
-
-
-var keys = document.querySelectorAll('.key');
-keys.forEach(function (key) {
-  key.addEventListener('transitionend', removeClass)
-})
-
-window.addEventListener('keydown', playSound);
+  window.addEventListener('keydown', playSound);
+}());
--- README.md
+++ README.md
@@ -6,3 +6,7 @@ No frameworks, no libraries, no wrappers... just pure VanillaJS ;)
 ## Table Of Contents
 
 1. [JavaScript Drum Kit](https://yogicat.github.io/JavaScript30/01-drumkit)
+
+    ![drum kit image](./screenshots/01-drumkit-resize.gif)
+
+
